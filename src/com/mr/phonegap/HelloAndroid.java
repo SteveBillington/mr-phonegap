17c7d5aa67a587c90a6ce102271484427b660a91
SteveBillington <sbillington@fry.com>
2012-10-01 08:30:49 -0400

    Initial Commit with empty base url

---
+++ src/com/mr/phonegap/HelloAndroid.java
@@ -0,0 +1,15 @@
+package com.mr.phonegap;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class HelloAndroid extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/PhoneGapRestActive.html");
+
+    }
+}
